0245281721d81f91373da04a563587ddcd3fa663
adibradu <adrian.bradu@gmail.com>
2022-06-03 14:04:08 +0300

    animation update

--- src/hooks/useFadeInFromBottom.jsx
+++ src/hooks/useFadeInFromBottom.jsx
@@ -9,9 +9,9 @@ const useFadeInFromBottom = (inView) => {
       ? fadeInFromBottom.start({
           opacity: 1,
           y: 0,
-          transition: { duration: 0.5 },
+          transition: { duration: 0.25 },
         })
-      : fadeInFromBottom.start({ opacity: 0, y: '75px' });
+      : fadeInFromBottom.start({ opacity: 0, y: '50px' });
   }, [inView, fadeInFromBottom]);
 
   return fadeInFromBottom;
